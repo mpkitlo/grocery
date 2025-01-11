73d0625e33229bbfde3b858e1268f38e57f62c83
Dominik Wawszczak <wawszczakd@gmail.com>
2025-01-11 18:12:44 +0100

    Removed DownloadApp and OurServices

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,6 @@
 import { Fade } from '@mui/material';
 import CustomersReview from './CustomersReview/CustomersReview';
-import DownloadOurApp from './DownloadOurApp/DownloadOurApp';
 import Hero from './Hero/Hero';
-import OurBestQualities from './OurBestQualities/OurBestQualities';
-import OurServices from './OurServices/OurServices';
 import PopularCategories from './PopularCategories/PopularCategories';
 import EnjoyOurFreshGroceryItems from './EnjoyOurFreshGroceryItems/EnjoyOurFreshGroceryItems';
 
@@ -16,11 +13,8 @@ const Home = () => {
       <main className='min-h-screen space-y-5 mb-9'>
         <Hero />
         <PopularCategories />
-        <OurBestQualities />
         <EnjoyOurFreshGroceryItems />
-        <OurServices />
         <CustomersReview />
-        <DownloadOurApp />
       </main>
     </Fade>
   )
